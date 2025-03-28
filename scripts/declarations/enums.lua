c185168a3bb80bd1158b19dbdf9cfbe33e21a21b
drpandacat <134325525+drpandacat@users.noreply.github.com>
2025-03-28 11:05:55 -0400

    creepy

--- scripts/declarations/enums.lua
+++ scripts/declarations/enums.lua
@@ -84,7 +84,63 @@ PlaceholderGlobal.Enum.Dict = {
         [CollectibleType.COLLECTIBLE_ANALOG_STICK] = true,
         [CollectibleType.COLLECTIBLE_EYE_OF_THE_OCCULT] = true,
         [CollectibleType.COLLECTIBLE_EPIC_FETUS] = true,
-    }
+    },
+    ---@type table<EffectVariant, true>
+    CREEP = {
+        [EffectVariant.CREEP_RED] = true,
+        [EffectVariant.CREEP_GREEN] = true,
+        [EffectVariant.CREEP_YELLOW] = true,
+        [EffectVariant.CREEP_WHITE] = true,
+        [EffectVariant.CREEP_BLACK] = true,
+        [EffectVariant.PLAYER_CREEP_LEMON_MISHAP] = true,
+        [EffectVariant.PLAYER_CREEP_HOLYWATER] = true,
+        [EffectVariant.PLAYER_CREEP_WHITE] = true,
+        [EffectVariant.PLAYER_CREEP_BLACK] = true,
+        [EffectVariant.PLAYER_CREEP_RED] = true,
+        [EffectVariant.PLAYER_CREEP_GREEN] = true,
+        [EffectVariant.PLAYER_CREEP_HOLYWATER_TRAIL] = true,
+        [EffectVariant.CREEP_BROWN] = true,
+        [EffectVariant.PLAYER_CREEP_LEMON_PARTY] = true,
+        [EffectVariant.PLAYER_CREEP_PUDDLE_MILK] = true,
+        [EffectVariant.CREEP_SLIPPERY_BROWN] = true,
+        [EffectVariant.CREEP_SLIPPERY_BROWN_GROWING] = true,
+        [EffectVariant.COLOSTOMIA_PUDDLE] = true,
+        [EffectVariant.CREEP_STATIC] = true,
+        [EffectVariant.CREEP_LIQUID_POOP] = true,
+    },
+    ---@type table<EffectVariant, true>
+    CREEP_SAFE = {
+        [EffectVariant.PLAYER_CREEP_LEMON_MISHAP] = true,
+        [EffectVariant.PLAYER_CREEP_HOLYWATER] = true,
+        [EffectVariant.PLAYER_CREEP_WHITE] = true,
+        [EffectVariant.PLAYER_CREEP_BLACK] = true,
+        [EffectVariant.PLAYER_CREEP_RED] = true,
+        [EffectVariant.PLAYER_CREEP_GREEN] = true,
+        [EffectVariant.PLAYER_CREEP_HOLYWATER_TRAIL] = true,
+        [EffectVariant.PLAYER_CREEP_LEMON_PARTY] = true,
+        [EffectVariant.PLAYER_CREEP_PUDDLE_MILK] = true,
+    },
+    ---@type table<EffectVariant, true>
+    CREEP_UNSAFE = {
+        [EffectVariant.CREEP_RED] = true,
+        [EffectVariant.CREEP_GREEN] = true,
+        [EffectVariant.CREEP_YELLOW] = true,
+        [EffectVariant.CREEP_WHITE] = true,
+        [EffectVariant.CREEP_BLACK] = true,
+        [EffectVariant.CREEP_SLIPPERY_BROWN] = true,
+        [EffectVariant.CREEP_SLIPPERY_BROWN_GROWING] = true,
+        [EffectVariant.CREEP_LIQUID_POOP] = true,
+        [EffectVariant.COLOSTOMIA_PUDDLE] = true,
+        [EffectVariant.CREEP_STATIC] = true,
+    },
+    ---@type table<EffectVariant, true>
+    CREEP_ELLIPSE = {
+        [EffectVariant.PLAYER_CREEP_HOLYWATER] = true,
+        [EffectVariant.PLAYER_CREEP_LEMON_MISHAP] = true,
+        [EffectVariant.PLAYER_CREEP_LEMON_PARTY] = true,
+        [EffectVariant.PLAYER_CREEP_PUDDLE_MILK] = true,
+        [EffectVariant.COLOSTOMIA_PUDDLE] = true,
+    },
 }
 
 ---@enum CardFilterFlag
