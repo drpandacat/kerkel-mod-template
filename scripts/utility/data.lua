d834f9a688311a07e75947d072382cfb5972d35c
drpandacat <pantherdash12@gmail.com>
2025-06-17 20:26:54 -0400

    post npc kill data supoert

--- scripts/utility/data.lua
+++ scripts/utility/data.lua
@@ -4,12 +4,22 @@ PlaceholderGlobal:AddPriorityCallback(ModCallbacks.MC_PRE_GAME_EXIT, CallbackPri
     end
 end)
 
+-- ---@param entity Entity
+-- PlaceholderGlobal:AddPriorityCallback(ModCallbacks.MC_POST_ENTITY_REMOVE, CallbackPriority.LATE, function (_, entity)
+--     local hash = GetPtrHash(entity)
+--     PlaceholderGlobal.Data.ENTITY[hash] = nil
+--     PlaceholderGlobal.Data.ENTITY_BACKUP[hash] = nil
+-- end)
+
 ---@param entity Entity
-PlaceholderGlobal:AddPriorityCallback(ModCallbacks.MC_POST_ENTITY_REMOVE, CallbackPriority.LATE, function (_, entity)
+local function PostEntityInit(_, entity)
     local hash = GetPtrHash(entity)
     PlaceholderGlobal.Data.ENTITY[hash] = nil
     PlaceholderGlobal.Data.ENTITY_BACKUP[hash] = nil
-end)
+end
+for _, v in ipairs(PlaceholderGlobal.Enum.Dict.POST_ENTITY_INIT) do
+    PlaceholderGlobal:AddPriorityCallback(v, CallbackPriority.IMPORTANT - 1, PostEntityInit)
+end
 
 if REPENTOGON then
     PlaceholderGlobal:AddPriorityCallback(ModCallbacks.MC_POST_GLOWING_HOURGLASS_SAVE, CallbackPriority.LATE, function ()
